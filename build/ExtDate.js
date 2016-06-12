6de33731e4fe7105d059076311f204e615ef5e68
Luís Rodrigues <netslider25@gmail.com>
2016-06-12 10:20:26 +0100

    Fixed the toString() method not being overriden with the Date.toString() method

--- build/ExtDate.js
+++ build/ExtDate.js
@@ -44,7 +44,7 @@
       for (var i = 0; i < propCount; i++) {
         propName = dateProps[i];
 
-        if (typeof this[propName] !== 'undefined' && propName !== 'toLocaleString') {
+        if (typeof this[propName] !== 'undefined' && propName !== 'toLocaleString' && propName !== 'toString') {
           continue;
         } else if (typeof Date.prototype[propName] === 'function') {
           this._addNativeMethod(propName);
--- src/ExtDate.js
+++ src/ExtDate.js
@@ -19,7 +19,11 @@ export default class ExtDate {
     for (let i = 0; i < propCount; i++) {
       propName = dateProps[i]
 
-      if (typeof this[propName] !== 'undefined' && propName !== 'toLocaleString') {
+      if (
+        typeof this[propName] !== 'undefined' &&
+        propName !== 'toLocaleString' &&
+        propName !== 'toString'
+      ) {
         continue
       } else if (typeof Date.prototype[propName] === 'function') {
         this._addNativeMethod(propName)
