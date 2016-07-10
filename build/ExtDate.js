d63e40fd3d2d96bb8ac73a3d60e64ec3dd1fa143
Luís Rodrigues <netslider25@gmail.com>
2016-07-10 12:34:20 +0100

    Added methods for setting the first day of the year

--- build/ExtDate.js
+++ build/ExtDate.js
@@ -25,14 +25,56 @@
     value: true
   });
   exports.default = ExtDate;
+  var extDateMethods = {
+    setFirstDayOfYear: function setFirstDayOfYear(year) {
+      this.setMilliseconds(0);
+      this.setSeconds(0);
+      this.setMinutes(0);
+      this.setHours(0);
+      this.setDate(1);
+      this.setMonth(0);
+
+      if (year) {
+        this.setFullYear(year);
+      }
+
+      return this;
+    },
+
+    setUTCFirstDayOfYear: function setUTCFirstDayOfYear(year) {
+      this.setUTCMilliseconds(0);
+      this.setUTCSeconds(0);
+      this.setUTCMinutes(0);
+      this.setUTCHours(0);
+      this.setUTCDate(1);
+      this.setUTCMonth(0);
+
+      if (year) {
+        this.setUTCFullYear(year);
+      }
+
+      return this;
+    }
+  };
+
+  function extend(src, dest) {
+    var keys = Object.keys(src);
+
+    keys.forEach(function (prop) {
+      dest[prop] = src[prop];
+    });
+
+    return dest;
+  }
+
   function ExtDate(year, month, day, hour, minutes, seconds, milliseconds) {
     if (!(this instanceof ExtDate)) {
-      throw new TypeError('Cannot call a class as a function');
+      return Date.apply(undefined, arguments);
     }
 
     var date = new (Function.prototype.bind.apply(Date, [null].concat(Array.prototype.slice.call(arguments))))();
 
-    return date;
+    return extend(extDateMethods, date);
   }
 
   var staticProps = Object.getOwnPropertyNames(Date);
--- src/ExtDate.js
+++ src/ExtDate.js
@@ -8,14 +8,93 @@
  * @license MIT
  */
 
+const extDateMethods = {
+  /**
+   * Sets the first day of the year for a specified date according to local time.
+   *
+   * @memberof ExtDate
+   * @public
+   * @param {!Number} [year] - If not specified, the year of the specified date will remain unchanged.
+   * @return {Date}
+   * @since 0.1.0
+   *
+   * @example
+   * dateInstance.setFirstDayOfYear(2016)
+   */
+  setFirstDayOfYear: function setFirstDayOfYear (year) {
+    this.setMilliseconds(0)
+    this.setSeconds(0)
+    this.setMinutes(0)
+    this.setHours(0)
+    this.setDate(1)
+    this.setMonth(0)
+
+    if (year) {
+      this.setFullYear(year)
+    }
+
+    return this
+  },
+
+  /**
+   * Sets the first day of the year for a specified date according to universal time.
+   *
+   * @memberof ExtDate
+   * @public
+   * @param {!Number} [year] - If not specified, the year of the specified date will remain unchanged.
+   * @return {Date}
+   * @since 0.1.0
+   *
+   * @example
+   * dateInstance.setUTCFirstDayOfYear(2016)
+   */
+  setUTCFirstDayOfYear: function setUTCFirstDayOfYear (year) {
+    this.setUTCMilliseconds(0)
+    this.setUTCSeconds(0)
+    this.setUTCMinutes(0)
+    this.setUTCHours(0)
+    this.setUTCDate(1)
+    this.setUTCMonth(0)
+
+    if (year) {
+      this.setUTCFullYear(year)
+    }
+
+    return this
+  }
+}
+
+/**
+ * Copy the properties of one object into another.
+ *
+ * @param {Object} src - The object to get the properties from.
+ * @param {Object} dest - The object to copy the properties into.
+ * @return {Object} - The extended object
+ * @since 0.1.0
+ */
+function extend (src, dest) {
+  const keys = Object.keys(src)
+
+  keys.forEach(function (prop) {
+    dest[prop] = src[prop]
+  })
+
+  return dest
+}
+
 /**
  * Creates an instance of ExtDate which implements new methods for
  * the Date object.
  *
- * @access public
+ * @function
+ * @constructs
+ * @public
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
- * @return {Date} - The Date instance with the added methods.
+ * @return {Date|String} - The Date instance with the added methods, or a string representation of the date if called as a function.
  * @since 0.1.0
+ *
+ * @example
+ * var currentDateTime = ExtDate()
  *
  * @example
  * new ExtDate();
@@ -32,9 +111,10 @@ export default function ExtDate (year, month, day, hour, minutes, seconds, milli
   // Create a Date instance to use internally
   const date = new Date(...arguments)
 
-  return date
+  return extend(extDateMethods, date)
 }
 
+// Extend ExtDate with Date's static methods
 const staticProps = Object.getOwnPropertyNames(Date)
 
 staticProps.forEach(function (prop) {
